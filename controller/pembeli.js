6f57ddec9fc1d5508ed0106c52cbf932640f969f
izun009 <izzudinwijaya19@gmail.com>
2019-06-17 13:06:04 +0700

    connection tested

--- controller/pembeli.js
+++ controller/pembeli.js
@@ -1,10 +1,10 @@
 // dashboard admin
 
-var connection = require('../config/db');
+var con = require('../config/db');
 var bcrypt = require('bcrypt');
 
 exports.allUsers = function (req,res) {
-    connection.query('SELECT * FROM pembeli', (err, rows) => {
+    con.query('SELECT * FROM pembeli', (err, rows) => {
         if (err){
             res.json({
                 status : 400,
@@ -22,7 +22,7 @@ exports.addUser = function (req, res) {
     var password = bcrypt.hashSync(req.body.password, 10);
     var saldo = req.body.saldo;
 
-    connection.query('INSERT INTO pembeli (email, password, saldo) values (?,?,?)',
+    con.query('INSERT INTO pembeli (email, password, saldo) values (?,?,?)',
     [email, password, saldo], (err, rows) => {
         if (err){
             res.json({
@@ -42,7 +42,7 @@ exports.addUser = function (req, res) {
 exports.findUser = function (req, res) {
     var id_pembeli = req.params.id_pembeli;
 
-    connection.query('SELECT * FROM pembeli WHERE id_pembeli = ? ', [id_pembeli], 
+    con.query('SELECT * FROM pembeli WHERE id_pembeli = ? ', [id_pembeli], 
     (err, rows) => {
         if (err){
             res.json({
@@ -63,7 +63,7 @@ exports.updateUser = function (req, res) {
     var password = bcrypt.hashSync(req.body.password, 10);
     var saldo = req.body.saldo;
 
-    connection.query('UPDATE pembeli SET email = ?, password = ?, saldo = ? WHERE id_pembeli = ?',
+    con.query('UPDATE pembeli SET email = ?, password = ?, saldo = ? WHERE id_pembeli = ?',
     [email, password, saldo, id_pembeli], (err, rows) => {
         if (err){
             res.json({
@@ -83,7 +83,7 @@ exports.updateUser = function (req, res) {
 exports.deleteUser = function (req, res) {
     var id_pembeli = req.body.id_pembeli;
 
-    connection.query('DELETE FROM pembeli WHERE id_pembeli = ?', 
+    con.query('DELETE FROM pembeli WHERE id_pembeli = ?', 
     [id_pembeli], (err, rows) => {
         if (err){
             tres.json({
