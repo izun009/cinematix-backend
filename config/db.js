fb281571bb48ab9fb2c685815955ca78c45387e9
izun009 <izzudinwijaya19@gmail.com>
2019-06-17 14:25:25 +0700

    Deployed

--- config/db.js
+++ config/db.js
@@ -12,12 +12,12 @@ function handleDisconnect() {
     connection.connect(function(err) {             
       if(err) {                                     
         console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); 
+        setTimeout(handleDisconnect, 4000); 
       }                                     
     });                                    
     connection.on('error', function(err) {
       console.log('db error', err);
-      if(err.code !== 'PROTOCOL_CONNECTION_LOST') { 
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
         handleDisconnect();                         
       } else {                                      
         throw err;                                  
