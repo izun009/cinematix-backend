02ddbb3f70e15981c98848430408fdb20c08c4e1
izun009 <izzudinwijaya19@gmail.com>
2019-06-17 14:36:35 +0700

    Deployed

--- config/db.js
+++ config/db.js
@@ -15,14 +15,6 @@ function handleDisconnect() {
         setTimeout(handleDisconnect, 4000); 
       }                                     
     });                                    
-    connection.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code !== 'PROTOCOL_CONNECTION_LOST') { 
-        handleDisconnect();                         
-      } else {                                      
-        throw err;                                  
-      }
-    });
 }
   
 handleDisconnect();
